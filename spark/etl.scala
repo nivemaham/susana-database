d88450a198e90076894bc7aceb2d90ac7bc155f0
parisni <nicolas.paris@riseup.net>
2018-12-27 17:35:05 +0100

    Add mapped names to to solr object

--- spark/etl.scala
+++ spark/etl.scala
@@ -1,4 +1,8 @@
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import java.util.Properties
+
 //
 // [ETL]
 //
@@ -30,6 +34,30 @@ object DbUtil {
     val connCfg = connStr.split(":")
     dbPassword(connCfg(0),connCfg(1),connCfg(2),connCfg(3))
   }
+
+  def getPgTable(url:String,table:String,partition_column:String,num_partitions:Int,properties:Properties):Dataset[Row]={
+    // Push aggregation to the database
+    val query = f"(SELECT cast(min($partition_column%s) as bigint), cast(max($partition_column%s) + 1 as bigint) FROM $table%s) AS tmp"
+    val row  = (spark.read.jdbc(url=url, table=query, properties=properties).first())
+    val lower_bound = row.getLong(0)
+    val upper_bound = row.getLong(1)
+    
+    //and pass to the main query:
+    spark.read.jdbc( url=url, table=table, columnName=partition_column, lowerBound=lower_bound, upperBound=upper_bound, numPartitions=num_partitions, connectionProperties=properties)
+    }
+
+  def getPgQuery(url:String,query:String,partition_column:String,num_partitions:Int,properties:Properties):Dataset[Row]={
+    // Push aggregation to the database
+    val queryStr = f"($query%s) as tmp"
+    val min_max_query = f"(SELECT cast(min($partition_column%s) as bigint), cast(max($partition_column%s) + 1 as bigint) FROM $queryStr%s) AS tmp1"
+    print(min_max_query)
+    val row  = (spark.read.jdbc(url=url, table=min_max_query, properties=properties).first())
+    val lower_bound = row.getLong(0)
+    val upper_bound = row.getLong(1)
+    
+    //and pass to the main query:
+    spark.read.jdbc( url=url, table=queryStr, columnName=partition_column, lowerBound=lower_bound, upperBound=upper_bound, numPartitions=num_partitions, connectionProperties=properties)
+    }
 }
 
 //
@@ -37,11 +65,14 @@ object DbUtil {
 // connect to postgres
 //
 
-val connectionStr = "jdbc:postgresql://localhost:5432/mimic?user=mapper&currentSchema=omopvocab"
-val prop = new java.util.Properties()
+val url = "jdbc:postgresql://localhost:5432/mimic?user=mapper&currentSchema=omopvocab"
+val prop = new Properties()
 prop.put("password", DbUtil.passwordFromConn("localhost:5432:mimic:mapper"))
-spark.read.jdbc(url=connectionStr,table="concept",columnName="concept_id",lowerBound=0,upperBound=4000000,numPartitions=8,connectionProperties=prop).registerTempTable("concept")
-spark.read.jdbc(url=connectionStr,table="concept_synonym",columnName="concept_id",lowerBound=0,upperBound=4000000,numPartitions=8,connectionProperties=prop).registerTempTable("concept_synonym")
+    
+DbUtil.getPgQuery(url, "select concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code from concept", "concept_id", 8, prop).registerTempTable("concept")
+DbUtil.getPgQuery(url, "select concept_id, concept_synonym_name from concept_synonym", "concept_id", 8, prop).registerTempTable("concept_synonym")
+DbUtil.getPgQuery(url, "select concept_id_1, concept_id_2 from concept_relationship where relationship_id = 'Maps to' and concept_id_1 != concept_id_2", "concept_id_1", 8, prop).registerTempTable("concept_relationship")
+
 
 //
 // [T]
@@ -57,6 +88,14 @@ spark.sql("""
    GROUP BY concept_id
 """).registerTempTable("synDF")
 
+spark.sql("""
+   SELECT cpt1.concept_id                 
+   ,collect_list(cpt2.concept_name) as concept_mapped_name
+   FROM concept_relationship as cr
+   JOIN concept as cpt1 on (cr.concept_id_1 = cpt1.concept_id)
+   JOIN concept as cpt2 on (cr.concept_id_2 = cpt2.concept_id)
+   GROUP BY cpt1.concept_id
+""").registerTempTable("mappedDF")
 
 val resultDF = spark.sql("""
    SELECT concept_id    
@@ -67,8 +106,10 @@ val resultDF = spark.sql("""
    , standard_concept    
    , concept_code        
    , concept_synonym_name
+   , concept_mapped_name
    FROM concept
-  LEFT JOIN synDF USING (concept_id)
+   LEFT JOIN synDF USING (concept_id)
+   LEFT JOIN mappedDF USING (concept_id)
 """)
 
 //
