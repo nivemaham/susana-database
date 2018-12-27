6bdfb967882e27f4b8e5e6296d36de2e0f127fbc
parisni <nicolas.paris@riseup.net>
2018-12-27 17:35:36 +0100

    Add mapped names to solr and python

--- python/client.py
+++ python/client.py
@@ -12,47 +12,43 @@ solr = pysolr.Solr('http://localhost:8983/solr/omop-concept')
 
 # do a search
 print("##\n# KEYWORD SEARCH\n##")
-results = solr.search('concept_name:stroke',  **{
+results = solr.search('concept_mapped_name:[* TO *]',  **{
     'hl': 'true',
-    'fl': 'id',
-    'hl.method': 'unified',
-    'termVectors': 'true',
-    'hl.fragsize': 10,
-    'hl.fl': 'concept_name',
-    'sort': 'score desc',
+    'fl': '*',
 })
 
 
 #first highlight
-#for (id, hl) in results.highlighting.items():
-#    print(f"{id} : {hl}")
-#
-#print(f"Saw {len(results.docs)} over {results.hits} results")
+for result in results.docs:
+    print(f"{result}")
+
+print(f"Saw {len(results.docs)} over {results.hits} results")
 
 # EDISMAX search
 # handle multiword synonyms
 # handle field query
 # handle boosting
 print("##\n# KEYWORD SEARCH\n##")
-results = solr.search('(suspected neck cancer) AND standard_concept:S',  **{
+results = solr.search('(Meningeal irritation) AND standard_concept:S',  **{
     'defType': 'edismax',
-    'fl': 'id,standard_concept',
-    'qf': 'concept_synonym_name^1 concept_name^2',
-    'pf3': 'concept_synonym_name^.3 concept_name^6',
+    'fl': '*',
+    'qf': 'concept_synonym_name^1 concept_name^2 concept_mapped_name^1',
+    'pf3': 'concept_synonym_name^3 concept_name^6 concept_mapped_name^3',
     'ps3': '2',
     'sort': 'score desc',
     'hl': 'true',
     'hl.method': 'unified',
     'termVectors': 'true',
     'hl.fragsize': 10,
-    'hl.fl': 'concept_name,concept_synonym_name',
+    'hl.snippets': 10,
+    'hl.requireFieldMatch': 'true',
+    'hl.fl': 'concept_name,concept_synonym_name,concept_mapped_name',
     'sow': 'false',
-    'rows': 10,
+    'rows': 35,
 })
 
 
-#first highlight
-for (id, hl) in results.highlighting.items():
-    print(f"{id} : {hl}")
+for result in results.docs:
+    print(f"{result} {results.highlighting.get(result['concept_id'])}" )
 
 print(f"Saw {len(results.docs)} over {results.hits} results")
