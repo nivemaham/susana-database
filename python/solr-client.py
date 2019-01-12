b665e8ecd6ceb3d6b8793ab0a3ff700c9f775d76
parisni <nicolas.paris@riseup.net>
2019-01-12 01:20:44 +0100

    Make use of solrCloud with pySolr

--- python/solr-client.py
+++ python/solr-client.py
@@ -1,35 +1,20 @@
 import pysolr
 
-def ifnull(var, val):
-  if var is None:
-    return val
-  return var
-
 # create a connection to a solr server
 #zookeeper = pysolr.ZooKeeper("localhost:9983")
 #solr = pysolr.SolrCloud(zookeeper, "gettingstarted")
-solr = pysolr.Solr('http://localhost:8983/solr/omop-concept')
+collection="omop-concept"
+pysolr.ZooKeeper.CLUSTER_STATE = f'/collections/{collection}/state.json'
+zookeeper = pysolr.ZooKeeper("localhost:9983")
+solrCloud = pysolr.SolrCloud(zookeeper,collection, results_cls=dict)
 
-# do a search
-print("##\n# KEYWORD SEARCH\n##")
-results = solr.search('concept_mapped_name:[* TO *]',  **{
-    'hl': 'true',
-    'fl': '*',
-})
-
-
-#first highlight
-#for result in results.docs:
-#    print(f"{result}")
-#
-#print(f"Saw {len(results.docs)} over {results.hits} results")
 
 # EDISMAX search
 # handle multiword synonyms
 # handle field query
 # handle boosting
 print("##\n# KEYWORD SEARCH\n##")
-results = solr.search('(Meningeal irritation) AND standard_concept:S',  **{
+results = solrCloud.search('(Meningeal irritation) AND standard_concept:S',  **{
     'defType': 'edismax',
     'fl': '*',
     'qf': 'concept_synonym_name^1 concept_name^2 concept_mapped_name^1',
@@ -44,12 +29,20 @@ results = solr.search('(Meningeal irritation) AND standard_concept:S',  **{
     'hl.requireFieldMatch': 'true',
     'hl.fl': 'concept_name,concept_synonym_name,concept_mapped_name',
     'sow': 'false',
-    'rows': 35,
+    'rows': 15,
     'wt': 'python',
+    'facet': 'true',
+    'facet.field': ['domain_id','standard_concept'],
+    'facet.limit': 5,
 })
 
 
-for result in results.docs:
-    print(f"{result} {results.highlighting.get(result['concept_id'])}" )
+for result in results["response"]["docs"]:
+    print(f"{result['concept_id']} | {result['domain_id']} | {results['highlighting'].get(result['concept_id'])['concept_name'][0]}" )
 
-print(f"Saw {len(results.docs)} over {results.hits} results")
+for res in results["facet_counts"]["facet_fields"]:
+    print(f"\nFACET {res}")
+    for facet in results["facet_counts"]["facet_fields"][res]:
+        print(f"{facet}")
+
+print(f"Saw {len(results['response']['docs'])} over {results['response']['numFound']} results")
