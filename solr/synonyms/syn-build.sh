fd7fddcb080879359285ce776e4536fe9203c760
parisni <nicolas.paris@riseup.net>
2019-02-17 17:23:25 +0100

    Add synonyms dictionnary for fr / fr-en

--- solr/synonyms/syn-build.sh
+++ solr/synonyms/syn-build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cat "solr/synonyms/synonyms-orig.txt" "solr/synonyms/synonyms-multi.txt" "solr/synonyms/synonyms-mono.txt" > "solr/configsets/omop/conf/synonyms.txt"
+cp solr/synonyms/synonyms-fr-en.txt solr/configsets/omop/conf/synonyms-fr-en.txt
+echo "" > solr/configsets/omop/conf/synonyms-fr.txt
