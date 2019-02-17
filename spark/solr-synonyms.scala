73bfff2a60ad4a74b8a9356d2a02b1b4fbb6372c
parisni <nicolas.paris@riseup.net>
2019-02-17 17:24:52 +0100

    Create synonyms from word embedding

--- spark/solr-synonyms.scala
+++ spark/solr-synonyms.scala
@@ -1,5 +1,10 @@
 
 spark.read.option("delimiter","\t").csv("private/mimic/dico_typo-50x50.txt").registerTempTable("dic")
-val dic = spark.sql("select _c1 || ' => ' || _c0 as resutl from dic")
-dic.write.text("private/syn.txt")
+val dic = spark.sql("""
+  SELECT   concat_ws(',',collect_list(distinct _c1)) as result
+  FROM     dic 
+  GROUP BY _c0 
+  HAVING   count(1) > 1
+  """)
+dic.coalesce(1).write.mode(org.apache.spark.sql.SaveMode.Overwrite).text("private/syn.txt")
 
