6872172f4e87c5cae67048d5e54ca4f9888084bd
parisni <nicolas.paris@riseup.net>
2019-02-17 17:25:45 +0100

    Calculate concept frequency and update concept

--- spark/etl-stats.scala
+++ spark/etl-stats.scala
@@ -1,5 +1,6 @@
 import fr.aphp.eds.spark.postgres.PGUtil
 import org.apache.spark.sql.{SparkSession,Dataset,Row}
+import scala.collection.mutable.ListBuffer
 
 def time[R](block: => R): R = {
     val t0 = System.nanoTime()
@@ -9,16 +10,16 @@ def time[R](block: => R): R = {
     result
 }
 
-def getConceptPercent(spark:SparkSession, url:String, tableStat:String, columnStat:String):Dataset[Row]={
+def getConceptPercent(pg:PGUtil, spark:SparkSession, url:String, tableStat:String, columnStat:String):Dataset[Row]={
   val statQuery = s"select $columnStat as concept_id from $tableStat"
-  val df = PGUtil.inputQueryBulkDf(spark, url, statQuery, "/tmp/test", false).registerTempTable("person")
-  val synDF = spark.sql("""
+  val df = pg.inputBulk(statQuery).registerTempTable("stats")
+  val query = """
   WITH
   tmp as (
       select 
         count(1) ct
-      , concept_id
-      from person
+      , concept_id 
+      from stats 
       group by concept_id
       ),
   total as (
@@ -29,11 +30,11 @@ def getConceptPercent(spark:SparkSession, url:String, tableStat:String, columnSt
   from tmp)
   SELECT
     concept_id
-  , ct
-  , CASE WHEN ct > p1 THEN 1 WHEN ct > p2 THEN 2 WHEN ct > p3 THEN 3 ELSE 4 END as percent
+  , CASE WHEN ct > p1 THEN 1 WHEN ct > p2 THEN 2 WHEN ct > p3 THEN 3 ELSE 4 END as m_frequency_value
   from tmp
   cross join total
-  """)
+  """
+  val synDF = spark.sql(query)
   synDF
 }
 
@@ -42,30 +43,34 @@ def getConceptPercent(spark:SparkSession, url:String, tableStat:String, columnSt
 // connect to postgres
 //
 
-val url = "jdbc:postgresql://localhost:5432/mimic?user=mapper&currentSchema=omop"
+val url = "jdbc:postgresql://localhost:5432/mimic?user=mapper&currentSchema=map"
+val pg = PGUtil(spark, url, "/home/natus/spark-postgres-tmp" )
 
 val strList = List(
-      List("person","gender_concept_id")
-     ,List("visit_occurrence","visit_concept_id")
-     ,List("visit_detail","visit_detail_concept_id")
-     ,List("observation","observation_concept_id")
-//   ,List("measurement","measurement_concept_id")
+      List("omop.person"               ,"gender_concept_id")
+     ,List("omop.visit_occurrence"     ,"visit_concept_id")
+     ,List("omop.visit_detail"         ,"visit_detail_concept_id")
+     ,List("omop.observation"          ,"observation_concept_id")
+     ,List("omop.measurement"          ,"measurement_concept_id")
+     ,List("omop.condition_occurrence" ,"condition_concept_id")
+     ,List("omop.procedure_occurrence" ,"procedure_concept_id")
+     ,List("omop.drug_exposure"        ,"drug_concept_id")
 );    
  
-def runJob():Unit = {
-  for(str <- strList){
-    val statDF = getConceptPercent(spark, url,  str(0), str(1))
-    statDF.show
-    PGUtil.outputBulkDfScd1(url, "m_concept_stats", "concept_id", statDF, 50000)
-  }
+var count = 0
+var query = new ListBuffer[String]() //empty mutable list
+for(str <- strList){
+  count = count + 1
+  val statDF = getConceptPercent(pg, spark, url,  str(0), str(1))
+  statDF.registerTempTable(f"stats$count")
+  query += f"(select * from stats$count)"
 }
-//PGUtil.outputBulkDfScd2(url, password, "concept_copy", "concept_id", "valid_start_date", "valid_end_date", someDF, 50000)
-  
-//time{
-//  PGUtil.getPgQuery(url,"select * from drug_exposure","drug_exposure_id",8,password).write.orc("/tmp/test1")
-//}
-//time{
-//  PGUtil.bulkInputDataframe(spark, url, password, "select * from drug_exposure", "/tmp/test", false)
-//}
+val queryUnion = query.mkString(" UNION ")
+val result = sql(queryUnion)
 
-//System.exit(0)
+pg.sqlExec("create table concept_tmp (concept_id integer not null, m_frequency_value smallint)")
+pg.outputBulk("concept_tmp", result, 2)
+pg.sqlExec("UPDATE concept set m_frequency_value = concept_tmp.m_frequency_value from concept_tmp where concept.concept_id = concept_tmp.concept_id")
+pg.tableDrop("concept_tmp")
+
+System.exit(0)
