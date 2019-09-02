ccfd24b99ffd94a7d0f5f0b7f6418df095b8b57f
parisni <nicolas.paris@riseup.net>
2019-09-02 11:20:03 +0200

    Add livy / solr wrappers

--- python/SparkLivySession.py
+++ python/SparkLivySession.py
@@ -1,6 +1,8 @@
 from livy import LivySession
 from livy.models import SessionKind
 import yaml
+import logging
+import sys
 
 
 class SparkLivySession():
@@ -13,8 +15,10 @@ class SparkLivySession():
     code_job: str = None
     code_init: str = None
     code_job: str = None
+    bullet_number: int = None
+    bullet: int = 0
 
-    def __init__(self,  yamlFilePath):
+    def __init__(self,  yamlFilePath, bullet_number=30):
         with open(yamlFilePath) as stream:
             conf = yaml.safe_load(stream)
         self.url = conf["url"]
@@ -23,18 +27,46 @@ class SparkLivySession():
         self.driver_memory = conf["driver_memory"]
         self.executor_memory = conf["executor_memory"]
         self.code_job = conf["code"]["job"]
+        self.bullet_number = bullet_number
         with open(conf["code"]["init"], 'r') as myfile:
             self.code_init = myfile.read()
 
-        self.session = LivySession(url=self.url, kind=SessionKind.SPARK, name=self.name, jars=self.jars, driver_memory=self.driver_memory, executor_memory=self.executor_memory)
 
-    def start(self):
+    def checkBullets(self):
+        logging.info("checking bullets")
+        if( self.bullet > self.bullet_number ):
+            logging.warning("too many bullets, dropping the session")
+            self.stop()
+            self.startSession()
+            self.bullet = 0
+
+    def checkState(self):
+        logging.info("checking state")
+        if( self.session is None or self.session.session_id is None ):
+            logging.info("No session yet ")
+            self.startSession()
+
+
+    def startSession(self):
+        logging.info("creating  a session")
+        self.session = LivySession(url=self.url, kind=SessionKind.SPARK, name=self.name, jars=self.jars, driver_memory=self.driver_memory, executor_memory=self.executor_memory)
         self.session.start()
         self.session.run(self.code_init)
 
     def run(self, *args):
+        self.checkBullets()
+        self.checkState()
         code_str = self.generate_run(*args)
-        self.session.run(code_str)
+        logging.info(code_str)
+        try:
+            logging.warning("running code: %s" % code_str)
+            self.session.run(code_str)
+            self.bullet = self.bullet + 1
+        except:
+            e = sys.exc_info()[0]
+            logging.error(e)
+            self.stop()
+            self.run(args)
 
     def run_arbitrary(self, code_str:str):
         try:
@@ -53,4 +85,9 @@ class SparkLivySession():
         return(f"{self.code_job}({argsStr})")
 
     def stop(self):
-        self.session.close()
+        try:
+            self.session.close()
+        except:
+            logging.warn("session already dead")
+
+        self.session = None
